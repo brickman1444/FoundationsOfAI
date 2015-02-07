d6933d0ac530d2315363ef014724dfda90406e3f
Zac <brickman1444@gmail.com>
2015-02-07 01:38:55 -0600

    final comments

--- danceBattle.py
+++ danceBattle.py
@@ -12,7 +12,7 @@ def print2DArray( array ):
 	for row in array:
 		print(row)
 
-# class for organizing the data about the problem
+# class for organizing the data about the problem such as the initial state
 class problem:
 
 	def __init__( self, n, m, inputTurns ):
@@ -39,39 +39,39 @@ class problem:
 
 		self.initialState = danceState(isMyTurn, self.lastTurn, usedTurns)
 
+# The state of the dance battle at some turn.
 class danceState:
 
 	def __init__( self, isMyTurn, turn, usedTurns ):
-		self.isMyTurn = isMyTurn # Identifying the person who performed the move to get here
-		self.turn = turn
-		self.prevMove = turn[1] # The last move in the turn used to get to this state
-		self.usedTurns = usedTurns
+		self.isMyTurn = isMyTurn	# Identifying the person who performed the moves on this turn
+		self.turn = turn			# list of the two moves performed on this turn
+		self.lastMove = turn[1]		# The last move in the turn
+		self.usedTurns = usedTurns	# the array of bools of what has and hasn't been used
 
+# unit of the tree constructed from all possible turns
 class node:
 	
 	def __init__(self, data, parent):
 		self.data = data # danceState
 		self.parent = parent
-		self.children = []
+		self.children = [] # populated by createChildFromMove()
 
+	# expands the tree to all possible nodes
 	def expand(self):
 
-		#print("trying to expand node")
-
 		for moveNumber in range(0,len(self.data.usedTurns)):
-			if self.data.usedTurns[self.data.prevMove][moveNumber] == False: # move has not been used
+			if self.data.usedTurns[self.data.lastMove][moveNumber] == False: # move has not been used
 
 				childNode = self.createChildFromMove( moveNumber )
 
 				childNode.expand()
 
-		#print("node expanded")
-
+	# create a child node for the state created from performing the specified move on the next turn
 	def createChildFromMove(self, moveNumber):
 
 		childUsedTurns = deepcopy(self.data.usedTurns)
 
-		childTurn = [ self.data.prevMove, moveNumber ]
+		childTurn = [ self.data.lastMove, moveNumber ]
 
 		markMoveAsDone( childUsedTurns, childTurn )
 
@@ -81,6 +81,7 @@ class node:
 
 		return childNode
 
+	# evaluates leaf nodes depending on who performed the move
 	def evaluate(self):
 
 		if len(self.children) == 0: # leaf node
@@ -95,7 +96,8 @@ class node:
 		else: # not leaf node
 
 			if not self.data.isMyTurn:
-				# look for max
+				# If it isn't currently my turn, then my turn is next and 
+				# I am looking for the max of the possible moves I can do
 
 				maxValue = None
 				self.childChosen = None
@@ -105,8 +107,7 @@ class node:
 
 					if maxValue is None or value > maxValue:
 						maxValue = value
-						#print("choosing node", childNode.data.turn)
-						self.childChosen = childNode
+						self.childChosen = childNode # for figuring out which path was chosen at the end
 
 				return maxValue
 
@@ -121,16 +122,13 @@ class node:
 
 					if minValue is None or value < minValue:
 						minValue = value
-						#print("choosing node", childNode.data.turn)
 						self.childChosen = childNode
 
 				return minValue
 
-
+# create a problem object for the given input file
 def readInProblem( fileName ):
 	file = open( fileName, "r")
-
-	#print(file.read())
 
 	n = int(file.readline())
 	m = int(file.readline())
@@ -163,13 +161,13 @@ rootNode = node( problemObj.initialState, None )
 
 #print("expanding nodes")
 
-rootNode.expand()
+rootNode.expand() # recursively expand all nodes
 
 #print("evaluating nodes")
 
-evaluation = rootNode.evaluate()
+evaluation = rootNode.evaluate() # recursively evaluate all nodes
 
-isMax = True
+isMax = True # track whose turn it is for the output
 
 for turn in problemObj.inputTurns:
 	if (isMax):
@@ -179,7 +177,7 @@ for turn in problemObj.inputTurns:
 	isMax = not isMax
 
 tempNode = rootNode
-tempNode = tempNode.childChosen
+tempNode = tempNode.childChosen # skip over the initial state so it isn't printed twice
 
 while tempNode is not None:
 	
