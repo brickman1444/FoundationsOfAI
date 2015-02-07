c5d3164e79733bebf328d833e1b63a0b9582d168
Zac <brickman1444@gmail.com>
2015-02-06 20:41:29 -0600

    File input and first test case read in correctly

--- danceBattle.py
+++ danceBattle.py
@@ -1,3 +1,44 @@
+def markMoveAsDone( boolTable, moveList ):
+	i = moveList[0]
+	j = moveList[1]
 
+	boolTable[i][j] = True
+	boolTable[j][i] = True
 
-print("hello world")
+file = open("danceTestCase1.txt", "r")
+
+#print(file.read())
+
+n = int(file.readline())
+m = int(file.readline())
+
+print("N = {0} M = {1}".format(n,m))
+
+inputMoves = []
+
+for inputLineIndex in range(0, m):
+
+	line = file.readline();
+	line = line.replace('\n','')
+
+	move = line.split(' ')
+
+	move[0] = int(move[0])
+	move[1] = int(move[1])
+
+	inputMoves.append(move)
+
+print(inputMoves)
+
+#Set up array of bools
+usedMoves = []
+
+emptyRow = [ False for x in range(0,n) ]
+
+for i in range(0,n):
+	usedMoves.append([ False for x in range(0,n) ])
+
+for move in inputMoves:
+	markMoveAsDone( usedMoves, move )
+
+print(usedMoves)
